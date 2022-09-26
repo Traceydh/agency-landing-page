32afa46ad817d2bab14f3b4661890e7627c1352f
tracey <tracey.d.huang@gmail.com>
2022-09-26 22:49:23 +1000

    Add JS for drop down menu button

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let hamburger = document.querySelector('#hamburger');
 hamburger.addEventListener('click', () => {
     console.log('click')
+    let dropDown = document.querySelector("#drop-down")
+    dropDown.classList.toggle('on');
 })
