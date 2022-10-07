1f5523d117696e4564a0521c58bdab8ab9bbd957
tracey <tracey.d.huang@gmail.com>
2022-10-07 23:45:57 +1100

    Remove drop down when user clicks elsewhere

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ let dropDown = document.querySelector("#drop-down")
 hamburger.addEventListener('click', () => {
     dropDown.classList.toggle('on');
 })
+
+window.onclick = function(event) {
+    if (!event.target.matches('img') && !dropDown.classList.contains('on')) {
+        dropDown.classList.toggle('on');
+    }
+}
